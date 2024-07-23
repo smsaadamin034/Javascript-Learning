e23b157edfc115a1ee62e0a4c78b720fb7715aa8
Saad Amin <smsaadamin034@gmail.com>
2024-07-23 16:02:49 +0500

    variables details added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 122445;
+let userName = "Saad Amin";
+let userEmail = "saad@gmail.com";
+var userDepartment = "Supply Chain";
+
+console.table([accountId,userName,userEmail,userDepartment]);
